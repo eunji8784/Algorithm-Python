100e1c02aa877b314774d9727056c729d6f40927
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2022-04-24 15:16:32 +0900

    220423

--- Baekjoon/1181-단어 정렬.py
+++ Baekjoon/1181-단어 정렬.py
@@ -1,1 +1,8 @@
 import sys
+n = int(sys.stdin.readline())
+wordList = [sys.stdin.readline().rstrip() for _ in range(n)]
+
+wordList1 = list(set(wordList))
+wordList1.sort()
+for word in sorted(wordList1, key=len):
+  print(word)
