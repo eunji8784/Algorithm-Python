500012f66e86ab3788a4f7ee77d55ecbbfd66fb3
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2022-04-26 13:27:34 +0900

    220426

--- SWEA/D1/2071-평균값 구하기.py
+++ SWEA/D1/2071-평균값 구하기.py
@@ -3,22 +3,3 @@ T = int(input())
 for t in range(T):
   numList = list(map(int, input().split()))
   print("#{} {}".format(t+1, round(sum(numList)/len(numList))))
-
-# solution 2  
-# T = int(input())
-# array = []
-
-# for _ in range(T):
-#   a, b = map(int, input().split())
-#   array.append([a, b])
-
-# t = 1
-# for a, b in array:
-#   if a > b:
-#     answer = ">"
-#   elif a < b:
-#     answer = "<"
-#   else:
-#     answer = "="
-#   print("#{} {}".format(t, answer))
-#   t += 1
