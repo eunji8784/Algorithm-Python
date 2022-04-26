b6938a9e6d74670cd298cfdb0308dc807bd280b8
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2022-04-26 14:36:52 +0900

    [REFACTOR] 220426

--- SWEA/D1/2072-홀수만 더하기.py
+++ SWEA/D1/2072-홀수만 더하기.py
@@ -1,15 +1,25 @@
-n = int(input())
+# n = int(input())
 
-array = []
-for _ in range(n):
-  numList = list(map(int, input().split()))
-  array.append(numList)
+# array = []
+# for _ in range(n):
+#   numList = list(map(int, input().split()))
+#   array.append(numList)
 
-t = 1
-for i in array:
+# t = 1
+# for i in array:
+#   sum = 0
+#   for j in range(len(i)):
+#     if i[j] % 2 == 1:
+#       sum += i[j]
+#   print("#"+ str(t), sum)
+#   t += 1
+
+T = int(input())
+array = [list(map(int, input().split())) for _ in range(T)]
+
+for t in range(T):
   sum = 0
-  for j in range(len(i)):
-    if i[j] % 2 == 1:
-      sum += i[j]
-  print("#"+ str(t), sum)
-  t += 1
+  for i in array[t]:
+    if i % 2 == 1:
+      sum += i
+  print("#{} {}".format(t + 1, sum))
