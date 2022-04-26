ddeaff343b7cf97dbf92e8f82f2a24b0f09550af
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2022-04-26 14:02:30 +0900

    [REFACTOR] use list comprehension in 220426

--- SWEA/D1/2068-최대수 구하기.py
+++ SWEA/D1/2068-최대수 구하기.py
@@ -1,11 +1,5 @@
 T = int(input())
-array = []
-
-for _ in range(T):
-  numList = list(map(int, input().split()))
-  array.append(numList)
-
-t = 1
-for a in array:
-  print("#{} {}".format(t, max(a)))
-  t += 1
+array = [list(map(int, input().split())) for _ in range(T)]
+ 
+for t in range(T):
+  print("#{} {}".format(t + 1, max(array[t])))
