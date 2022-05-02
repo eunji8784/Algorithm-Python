76d55baac3e521d965901d8b85137e492983537a
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2022-05-02 20:59:04 +0900

    220502

--- Baekjoon/2605-줄 세우기.py
+++ Baekjoon/2605-줄 세우기.py
@@ -6,6 +6,6 @@ num_list = list(map(int, input().split()))
 
 array = []
 for i in range(student_num):
-    array.insert(i - num_list[i], str(i + 1)
+    array.insert(i - num_list[i], str(i + 1))
 
 print(" ".join(array))
