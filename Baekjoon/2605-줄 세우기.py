e945bd6e16e50df5e90dde68ae10a0d6fd43051c
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2022-05-02 20:58:30 +0900

    220502

---
+++ Baekjoon/2605-줄 세우기.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+student_num = int(input())
+num_list = list(map(int, input().split()))
+
+array = []
+for i in range(student_num):
+    array.insert(i - num_list[i], str(i + 1)
+
+print(" ".join(array))
