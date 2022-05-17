4effd3997820e11166e7fb7a309338d18685a539
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2022-05-18 00:37:20 +0900

    220517

--- SWEA/D3/1209.py
+++ SWEA/D3/1209.py
@@ -1,36 +1,37 @@
 T = 10
+N = 100
 
 for test_case in range(1, T + 1):
-    N = int(input())
-    array = [list(map(int, input().split())) for _ in range(100)]
+    t = int(input())
+    array = [list(map(int, input().split())) for _ in range(N)]
     max_v = array[0][0]
 
     # 행의 합
-    for i in range(100):
+    for i in range(N):
         sum_v = sum(array[i])
         if sum_v > max_v:
             max_v = sum_v
 
     # 열의 합
-    for i in range(100):
+    for i in range(N):
         sum_v = 0
-        for j in range(100):
+        for j in range(N):
             sum_v += array[j][i]
         if sum_v > max_v:
             max_v = sum_v
 
     sum_v = 0
     # 오른쪽 아래 방향 대각선의 합
-    for i in range(100):
+    for i in range(N):
         sum_v += array[i][j]
     if sum_v > max_v:
         max_v = sum_v
 
     sum_v = 0
     # 왼쪽 아래 방향 대각선의 합
-    for i in range(100):
-        sum_v += array[i][99 - i]
+    for i in range(N):
+        sum_v += array[i][N - 1 - i]
     if sum_v > max_v:
         max_v = sum_v
 
-    print("#{} {}".format(N, max_v))
+    print("#{} {}".format(t, max_v))
