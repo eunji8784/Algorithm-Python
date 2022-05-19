2ed728cb05b91138cafc625c1a656d17543cd6f5
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2022-05-19 18:56:42 +0900

    220519

---
+++ SWEA/D3/1229.py
@@ -0,0 +1,23 @@
+T = 10
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    origin = list(input().split())
+    M = int(input())
+    command = list(input().split())
+
+    for i in range(len(command)):
+        if command[i] == 'I':
+            idx = int(command[i + 1])
+            num = int(command[i + 2])
+            for _ in range(num):
+                origin.insert(idx, command[i + 3])
+                i += 1
+                idx += 1
+        if command[i] == 'D':
+            idx = int(command[i + 1])
+            num = int(command[i + 2])
+            for _ in range(num):
+                origin.pop(idx)
+
+    print("#{} {}".format(test_case, " ".join(origin[:10])))
