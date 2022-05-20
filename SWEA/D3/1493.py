2857de739a4858e670a948f5c0b5c46dcac2ef01
Eunji Kim <70746467+eunji8784@users.noreply.github.com>
2022-05-20 18:31:59 +0900

    220520

---
+++ SWEA/D3/1493.py
@@ -0,0 +1,31 @@
+T = int(input())
+N = 300
+ 
+for test_case in range(1, T + 1):
+    p, q = map(int, input().split())
+    array = [[0] * N for _ in range(N)]
+ 
+    for i in range(1, N):
+        array[i][1] = array[i - 1][1] + i
+ 
+    for i in range(1, N):
+        k = i
+        for j in range(2, N):
+            array[i][j] = array[i][j - 1] + k
+            k += 1
+ 
+    xp = yp = xq = yq = 0
+    for i in range(1, N):
+        for j in range(1, N):
+            if array[i][j] == p:
+                xp = i
+                yp = j
+            if array[i][j] == q:
+                xq = i
+                yq = j
+            if xp * yp * xq * yq != 0:
+                break
+ 
+    res = array[xp + xq][yp + yq]
+ 
+    print("#{} {}".format(test_case, res))
